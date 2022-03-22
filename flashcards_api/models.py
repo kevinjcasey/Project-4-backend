1372ccfef51ad88a7b92b7ec28b5c60488af3249
Kevin Casey <kevinjcasey@kevins-air-m1.myfiosgateway.com>
2022-03-21 22:32:41 -0400

    Updated max character length

--- flashcards_api/models.py
+++ flashcards_api/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class Flashcard(models.Model):
     subject = models.CharField(max_length=32)
-    question = models.CharField(max_length=32)
-    answer = models.CharField(max_length=32)
+    question = models.CharField(max_length=255)
+    answer = models.CharField(max_length=255)
