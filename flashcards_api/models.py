bbf5e2bad7b24b870d369ceeae3f0e6b2bd162a6
Kevin Casey <kevinjcasey@kevins-air-m1.myfiosgateway.com>
2022-03-21 22:52:39 -0400

    Changed model to TextField

--- flashcards_api/models.py
+++ flashcards_api/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Flashcard(models.Model):
-    subject = models.CharField(max_length=1000)
-    question = models.CharField(max_length=1000)
-    answer = models.CharField(max_length=1000)
+    subject = models.TextField(max_length=1000)
+    question = models.TextField(max_length=1000)
+    answer = models.TextField(max_length=1000)
